553adcb1a3eb80079a8428c24696930f089b4e2b
chanksela <kakhachankseliani@gmail.com>
2022-04-14 07:41:16 +0400

    connected frontedn and backend

--- part3/phonebook-frontend/src/services/persons.js
+++ part3/phonebook-frontend/src/services/persons.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const baseURL = "http://localhost:3001/persons";
+const baseURL = "http://localhost:3001/api/persons";
 const getAll = () => {
   const request = axios.get(baseURL);
   return request.then((res) => res.data);
@@ -13,7 +13,7 @@ const removePerson = (id) => {
   return request.then((res) => res.data);
 };
 const updateInfo = (id, newPerson) => {
-  const request = axios.put(`http://localhost:3001/persons/${id}`, newPerson);
+  const request = axios.put(`${baseURL}/${id}`, newPerson);
   return request.then((res) => res.data);
 };
 export default { getAll, addPerson, removePerson, updateInfo };
